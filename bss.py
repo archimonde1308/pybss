39037718e5bc1afe56990ce6e3b1070f17bb056c
Kevin Brown <kevin.s.brown@uconn.edu>
2017-06-20 12:49:31 -0400

    Bugfix in AMUSE; bump to 0.0.10
    Former-commit-id: 24a239b2ed215593fd477210303ab717363b34d5

--- bss.py
+++ bss.py
@@ -75,7 +75,7 @@ def amuse(X, tau = 1):
     Y = dot(C,X)
     t = X.shape[1]
 
-    Ry = linalg.lagged_covariance(Y,0)
+    Ry = linalg.lagged_covariance(Y,tau)[tau]
     Ry = Ry[0]
 
     uy,sy,vy = svd((Ry + Ry.T)/2, full_matrices=False)
